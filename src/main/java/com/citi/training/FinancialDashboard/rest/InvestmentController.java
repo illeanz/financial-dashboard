38867bcaab6244ceb6db65efdf7c354615ee3d45
Aishwarya <aishwarya.abhiram.aa@gmail.com>
2021-08-25 22:40:32 +0000

    CrossOrigin added

--- src/main/java/com/citi/training/FinancialDashboard/rest/InvestmentController.java
+++ src/main/java/com/citi/training/FinancialDashboard/rest/InvestmentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 import java.util.Date;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/investments")
 public class InvestmentController {
